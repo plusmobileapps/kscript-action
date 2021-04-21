1cba442b194924831a2a1b5d7b5c40f3e58682ad
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-21 09:36:17 -0700

    update docker file for running kscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
 
-curl -s "https://get.sdkman.io" | bash     # install sdkman
-source "$HOME/.sdkman/bin/sdkman-init.sh"  # add sdkman to PATH
+source /root/.sdkman/bin/sdkman-init.sh
+/usr/bin/env kscript ktest.kts /github/workspace/$1
 
-sdk install kotlin  
-sdk install kscript
-
-kscript ktest.kts /github/workspace/$1
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
