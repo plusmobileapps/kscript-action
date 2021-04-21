8630773b90e2040f9ef158513984411624e7c0aa
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-21 09:22:34 -0700

    install the dependencies needed to run kscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+curl -s "https://get.sdkman.io" | bash     # install sdkman
+source "$HOME/.sdkman/bin/sdkman-init.sh"  # add sdkman to PATH
+
+sdk install kotlin  
+sdk install kscript
+
 kscript ktest.kts /github/workspace/$1
 echo "Hello $1"
 time=$(date)
