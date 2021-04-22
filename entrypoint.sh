99e8266f7b33a74fcce334280afae0c6538eaab7
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-22 15:45:33 -0700

    only have kscript command run in shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 kscript /ktest.kts /github/workspace/$1
 
-if [[ $? -eq 0 ]]; then
-    echo "Tests passed. Do something."
-else
-    echo "Tests didn't pass. Do something."
-    exit 1
-fi
+# if [[ $? -eq 0 ]]; then
+#     echo "Tests passed. Do something."
+# else
+#     echo "Tests didn't pass. Do something."
+#     exit 1
+# fi
