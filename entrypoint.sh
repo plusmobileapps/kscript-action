24ccc1c1e75883fc610211ea7fef0f4fcfbada4c
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-22 12:51:18 -0700

    rename names in action meta data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
 
-# export PATH=$PATH:/usr/local/sdkman/candidates/java/current/bin/java
-# export PATH=$PATH:/usr/local/sdkman/candidates/kotlin/current/bin/kotlin
-# export PATH=$PATH:/usr/local/sdkman/candidates/kscript/current/bin/kscript
-# export PATH=$PATH:/usr/local/sdkman/candidates/gradle/current/bin/gradle
-
 kscript /ktest.kts /github/workspace/$1
 
 if [[ $? -eq 0 ]]; then
@@ -13,7 +8,3 @@ else
     echo "Tests didn't pass. Do something."
     exit 1
 fi
-
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
