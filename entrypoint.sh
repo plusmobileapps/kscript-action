d6c48f74be70ec84b14b106d2b473333466ae15a
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-25 15:32:06 -0700

    remove adding kscript to path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,6 @@ source "$SDKMAN_DIR/bin/sdkman-init.sh"
 # install kscript with the provided version 
 sdk install kscript $2
 
-# add kscript to the path 
-export PATH=$PATH:/usr/local/sdkman/candidates/kscript/current/bin
-
 # run the tests with the helper script
 # the project should be checked out at /github/workspace/ with the checkout action https://github.com/actions/checkout
 kscript /ktest.kts /github/workspace/$1
