a5fc324f721e659a0475cedcb0f5c4c1f562b535
Andrew Steinmetz <andrew@plusmobileapps.com>
2021-04-27 23:09:12 -0700

    install kscript last in shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ KOTLIN_VERSION=$4
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 # install kscript & dependencies with the provided versions
-sdk install kscript $KTS_FILE
 sdk install java $JAVA_VERSION
 sdk install kotlin $KOTLIN_VERSION
+sdk install kscript $KTS_FILE
 
 # run the tests with the helper script
 # the project should be checked out at /github/workspace/ with the checkout action https://github.com/actions/checkout
